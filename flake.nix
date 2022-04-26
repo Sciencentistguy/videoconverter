3f8727bfc436da0d13dc73dbe431649709e3f125
Jamie Quigley <jamie@quigley.xyz>
2022-04-26 16:30:55 +0100

    Clean up flake

--- flake.nix
+++ flake.nix
@@ -4,30 +4,58 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
-      url = github:edolstra/flake-compat;
+      url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, flake-utils, ... }: {
-    overlay = final: prev:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    {
+      overlay = final: prev: {
+        videoconverter = self.packages.${prev.system}.default;
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem (system:
+      # AFAICT I have to instantiate a nixpkgs here because of the unfree, even though it is le bad
       let
         pkgs = import "${nixpkgs}" {
-          config = { allowUnfree = true; };
-          system = prev.system;
+          config.allowUnfree = true;
+          inherit system;
         };
-      in
-      {
-        videoconverter = pkgs.callPackage ./videoconverter.nix { };
-      };
-  } //
-  flake-utils.lib.eachDefaultSystem (system:
-    # AFAICT I have to instantiate a nixpkgs here because of the unfree, even though it is le bad
-    let pkgs = import "${nixpkgs}" {
-      config = { allowUnfree = true; };
-      inherit system;
-    };
-    in
-    {
-      packages.default = pkgs.callPackage ./videoconverter.nix { };
-    });
+        inherit (pkgs) lib rustPlatform;
+      in {
+        packages.default = let
+          ffmpeg-fdk = pkgs.ffmpeg-full.override {
+            nonfreeLicensing = true;
+            fdkaacExtlib = true;
+          };
+        in
+          rustPlatform.buildRustPackage {
+            pname = "videoconverter";
+            version = "0.2.1";
+            src = lib.cleanSource ./.;
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            prePatch = ''
+              substituteInPlace src/backend.rs --replace 'const FFMPEG_BIN_PATH: &str = "ffmpeg";' 'const FFMPEG_BIN_PATH: &str = "${ffmpeg-fdk}/bin/ffmpeg";'
+            '';
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              rustPlatform.bindgenHook
+              clippy
+            ];
+
+            buildInputs = [
+              ffmpeg-fdk
+            ];
+
+            RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+          };
+      });
 }
