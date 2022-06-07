9d4ba16027bcee0559dc0edef4d273b0018a7efe
Jamie Quigley <jamie@quigley.xyz>
2022-06-07 19:37:08 +0100

    Revert "Update flake to use nixpkgs-unfree"
    This reverts commit c365c4ce5e2bb907dcd0753080aee5f6b8fbbb95.

--- flake.nix
+++ flake.nix
@@ -1,24 +1,16 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    };
-    nixpkgs-unfree = {
-      url = "github:numtide/nixpkgs-unfree";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    # github example, also supported gitlab:
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
   };
   outputs = {
     self,
-    nixpkgs-unfree,
+    nixpkgs,
     flake-utils,
     ...
   }:
@@ -27,58 +19,64 @@
         videoconverter = self.packages.${prev.system}.default;
       };
     }
-    // flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs-unfree.legacyPackages.${system};
-      videoconverter = {
-        rustPlatform,
-        lib,
-        pkg-config,
-        ffmpeg,
-      }:
-        rustPlatform.buildRustPackage {
-          pname = "videoconverter";
-          version = "0.2.1";
-          src = lib.cleanSource ./.;
+    // flake-utils.lib.eachDefaultSystem (system:
+      # AFAICT I have to instantiate a nixpkgs here because of the unfree, even though it is le bad
+      let
+        pkgs = import "${nixpkgs}" {
+          config.allowUnfree = true;
+          inherit system;
+        };
+        inherit (pkgs) lib rustPlatform;
+        videoconverter = {
+          rustPlatform,
+          lib,
+          pkg-config,
+          ffmpeg,
+        }:
+          rustPlatform.buildRustPackage {
+            pname = "videoconverter";
+            version = "0.2.1";
+            src = lib.cleanSource ./.;
 
-          cargoLock.lockFile = ./Cargo.lock;
+            cargoLock.lockFile = ./Cargo.lock;
 
-          prePatch = ''
-            substituteInPlace src/backend.rs --replace 'const FFMPEG_BIN_PATH: &str = "ffmpeg";' 'const FFMPEG_BIN_PATH: &str = "${ffmpeg}/bin/ffmpeg";'
-          '';
+            prePatch = ''
+              substituteInPlace src/backend.rs --replace 'const FFMPEG_BIN_PATH: &str = "ffmpeg";' 'const FFMPEG_BIN_PATH: &str = "${ffmpeg}/bin/ffmpeg";'
+            '';
 
-          nativeBuildInputs = [
-            pkg-config
-            rustPlatform.bindgenHook
-          ];
+            nativeBuildInputs = [
+              pkg-config
+              rustPlatform.bindgenHook
+            ];
 
-          buildInputs = [
-            ffmpeg
-          ];
-          meta = with lib; {
-            license = licenses.mpl20;
-            homepage = "https://github.com/Sciencentistguy/videoconverter";
-            platforms = ffmpeg.meta.platforms;
+            buildInputs = [
+              ffmpeg
+            ];
+            meta = with lib; {
+              license = licenses.mpl20;
+              homepage = "https://github.com/Sciencentistguy/videoconverter";
+              platforms = ffmpeg.meta.platforms;
+            };
+          };
+      in {
+        packages.videoconverter = pkgs.callPackage videoconverter {
+          ffmpeg = pkgs.ffmpeg-full.override {
+            nonfreeLicensing = true;
+            fdkaacExtlib = true;
           };
         };
-    in {
-      packages.videoconverter = pkgs.callPackage videoconverter {
-        ffmpeg = pkgs.ffmpeg-full.override {
-          nonfreeLicensing = true;
-          fdkaacExtlib = true;
-        };
-      };
 
-      packages.default = self.packages.${system}.videoconverter;
+        packages.default = self.packages.${system}.videoconverter;
 
-      devShells.default = self.packages.${system}.videoconverter.overrideAttrs (super: {
-        nativeBuildInputs = with pkgs;
-          super.nativeBuildInputs
-          ++ [
-            clippy
-            rustfmt
-            cargo-edit
-          ];
-        RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+        devShells.default = self.packages.${system}.default.overrideAttrs (super: {
+          nativeBuildInputs = with pkgs;
+            super.nativeBuildInputs
+            ++ [
+              clippy
+              rustfmt
+              cargo-edit
+            ];
+          RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+        });
       });
-    });
 }
