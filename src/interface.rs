3344751520f14dd6e41907efdfd155f5b462e6c7
Jamie Quigley <jamie@quigley.xyz>
2022-10-29 14:03:06 +0100

    Clean up TV mode interactive menu

--- src/interface.rs
+++ src/interface.rs
@@ -166,59 +166,77 @@ pub struct TVOptions {
     pub episode: usize,
 }
 
+fn transpose_state(x: Option<TVOptions>) -> (Option<String>, Option<usize>, Option<usize>) {
+    match x {
+        Some(TVOptions {
+            title,
+            season,
+            episode,
+        }) => (Some(title), Some(season), Some(episode)),
+        None => (None, None, None),
+    }
+}
+
 pub fn get_tv_options() -> Option<TVOptions> {
     let enabled = ARGS.tv_mode || util::confirm("TV Show Mode", Some(Answer::NO));
     if !enabled {
         return None;
     }
 
-    let mut previous_state = state::read_state();
-    let mut title = String::new();
+    let previous_state = state::read_state();
+    let mut still_using_previous = false;
 
-    if let Some(ref mut previous_state) = previous_state {
-        let use_old_value = util::confirm(
-            &format!("Use previous title? ({})", previous_state.title),
-            None,
-        );
+    let (previous_title, previous_season, _) = transpose_state(previous_state);
 
-        if use_old_value {
-            title = std::mem::take(&mut previous_state.title);
-        }
-    }
+    let title = {
+        let mut title = String::new();
+        if let Some(previous_title) = previous_title {
+            let is_blank = previous_title.is_empty();
+            let use_old_value = (!is_blank)
+                && util::confirm(&format!("Use previous title? ({})", previous_title), None);
 
-    if title.is_empty() {
-        title = loop {
-            let response = util::prompt("Please enter the title of the TV show:");
-            if !response.is_empty() {
-                break response;
+            still_using_previous = use_old_value;
+            if use_old_value {
+                title = previous_title;
             }
         }
-    }
 
-    let mut season = None;
-
-    if let Some(previous_state) = previous_state {
-        let use_old_value = util::confirm(
-            &format!("Use previous season? ({})", previous_state.season),
-            None,
-        );
-
-        if use_old_value {
-            season = Some(previous_state.season);
-        }
-    }
-
-    if season.is_none() {
-        season = loop {
-            match util::prompt("Enter the season index of the TV show:").parse::<usize>() {
-                Ok(x) => break Some(x),
-                Err(_) => {
-                    println!("Invalid response. Please try again.");
-                    continue;
+        if title.is_empty() {
+            title = loop {
+                let response = util::prompt("Please enter the title of the TV show:");
+                if !response.is_empty() {
+                    break response;
                 }
             }
         }
-    }
+        title
+    };
+
+    let season = {
+        let mut season = None;
+
+        if let Some(previous_season) = previous_season {
+            let use_old_value = still_using_previous
+                && util::confirm(&format!("Use previous season? ({})", previous_season), None);
+
+            if use_old_value {
+                season = Some(previous_season);
+            }
+        }
+
+        if season.is_none() {
+            season = loop {
+                match util::prompt("Enter the season index of the TV show:").parse::<usize>() {
+                    Ok(x) => break Some(x),
+                    Err(_) => {
+                        println!("Invalid response. Please try again.");
+                        continue;
+                    }
+                }
+            }
+        }
+        season.unwrap()
+    };
 
     let episode = loop {
         if let Ok(x) =
@@ -230,7 +248,7 @@ pub fn get_tv_options() -> Option<TVOptions> {
 
     Some(TVOptions {
         title,
-        season: season.unwrap(),
+        season,
         episode,
     })
 }
