96821d1663bf67b944b215b1e9443d892144c81a
Jamie Quigley <jamie@quigley.xyz>
2022-11-19 15:03:19 +0000

    Use crane in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,15 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    crane,
     ...
   }:
     {
@@ -26,36 +25,28 @@
           config.allowUnfree = true;
           inherit system;
         };
-        inherit (pkgs) lib rustPlatform;
-        videoconverter = {
-          rustPlatform,
-          lib,
-          pkg-config,
-          ffmpeg,
-        }:
-          rustPlatform.buildRustPackage {
-            name = "videoconverter";
-            src = lib.cleanSource ./.;
+        craneLib = crane.lib.${system};
 
-            cargoLock.lockFile = ./Cargo.lock;
+        videoconverter = {ffmpeg}:
+          craneLib.buildPackage {
+            name = "videoconverter";
+            src = pkgs.lib.cleanSource ./.;
 
             # Point to a nixpkgs ffmpeg rather than using the one on $PATH
             prePatch = let
-              nnedi_weights = builtins.fetchurl {
-                url = "https://github.com/dubhater/vapoursynth-nnedi3/raw/cc6f6065e09c9241553cb51f10002a7314d66bfa/src/nnedi3_weights.bin";
-                sha256 = "0hhx4n19qaj3g68f5kqjk23cj063g4y2zidivq9pdfrm0i1q5wr7";
-              };
             in ''
-              substituteInPlace src/ffmpeg_backend.rs \
-                --replace 'const FFMPEG_BIN_PATH: &str = "ffmpeg";'\
-                          'const FFMPEG_BIN_PATH: &str = "${ffmpeg}/bin/ffmpeg";'
+              if [ -f ./src/ffmpeg_backend.rs ]; then
+                substituteInPlace src/ffmpeg_backend.rs \
+                  --replace 'const FFMPEG_BIN_PATH: &str = "ffmpeg";'\
+                            'const FFMPEG_BIN_PATH: &str = "${ffmpeg}/bin/ffmpeg";'
 
-              substituteInPlace src/interface.rs \
-                --replace '"~/.ffmpeg/nnedi3_weights"'\
-                          '"${nnedi_weights}"'
+                substituteInPlace src/interface.rs \
+                  --replace '"~/.ffmpeg/nnedi3_weights"'\
+                            '"${self.outputs.packages.${system}.nnedi_weights}"'
+              fi
             '';
 
-            nativeBuildInputs = [
+            nativeBuildInputs = with pkgs; [
               pkg-config
               rustPlatform.bindgenHook
             ];
@@ -66,19 +57,25 @@
 
             inherit ffmpeg;
 
-            meta = with lib; {
+            meta = with pkgs.lib; {
               license = licenses.mpl20;
               homepage = "https://github.com/Sciencentistguy/videoconverter";
               platforms = ffmpeg.meta.platforms;
             };
           };
       in {
+        packages.nnedi_weights = builtins.fetchurl {
+          url = "https://github.com/dubhater/vapoursynth-nnedi3/raw/cc6f6065e09c9241553cb51f10002a7314d66bfa/src/nnedi3_weights.bin";
+          sha256 = "0hhx4n19qaj3g68f5kqjk23cj063g4y2zidivq9pdfrm0i1q5wr7";
+        };
+
         packages.videoconverter = pkgs.callPackage videoconverter {
           ffmpeg = pkgs.ffmpeg_5-full.override {
             nonfreeLicensing = true;
             fdkaacExtlib = true;
           };
         };
+
         # Assume that ffmpeg-full works and I don't need to build it in CI
         packages.videoconverter-ci = pkgs.callPackage videoconverter {
           ffmpeg = pkgs.ffmpeg_5;
@@ -93,8 +90,10 @@
               clippy
               rustfmt
               cargo-edit
+              rustc
             ];
-          RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+
+          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
         });
       });
 }
