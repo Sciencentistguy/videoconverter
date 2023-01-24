a17bb402f7e37a7fae88b3c36f6dc6642296fa84
Jamie Quigley <jamie@quigley.xyz>
2023-01-24 20:43:33 +0000

    Revert "Use crane in flake.nix"
    This reverts commit 96821d1663bf67b944b215b1e9443d892144c81a.
    
    Apparently this broke the nnedi_weights patching, and I only just
    noticed. /shrug

--- flake.nix
+++ flake.nix
@@ -2,14 +2,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    crane,
     ...
   }:
     {
@@ -25,28 +26,37 @@
           config.allowUnfree = true;
           inherit system;
         };
-        craneLib = crane.lib.${system};
+        inherit (pkgs) lib rustPlatform;
 
-        videoconverter = {ffmpeg}:
-          craneLib.buildPackage {
+        nnedi_weights = pkgs.fetchurl {
+          url = "https://github.com/dubhater/vapoursynth-nnedi3/raw/cc6f6065e09c9241553cb51f10002a7314d66bfa/src/nnedi3_weights.bin";
+          sha256 = "0hhx4n19qaj3g68f5kqjk23cj063g4y2zidivq9pdfrm0i1q5wr7";
+        };
+
+        videoconverter = {
+          rustPlatform,
+          lib,
+          pkg-config,
+          ffmpeg,
+        }:
+          rustPlatform.buildRustPackage {
             name = "videoconverter";
-            src = pkgs.lib.cleanSource ./.;
+            src = lib.cleanSource ./.;
+
+            cargoLock.lockFile = ./Cargo.lock;
 
             # Point to a nixpkgs ffmpeg rather than using the one on $PATH
-            prePatch = let
-            in ''
-              if [ -f ./src/ffmpeg_backend.rs ]; then
-                substituteInPlace src/ffmpeg_backend.rs \
-                  --replace 'const FFMPEG_BIN_PATH: &str = "ffmpeg";'\
-                            'const FFMPEG_BIN_PATH: &str = "${ffmpeg}/bin/ffmpeg";'
+            prePatch = ''
+              substituteInPlace src/command.rs \
+                --replace 'const FFMPEG_BIN_PATH: &str = "ffmpeg";'\
+                          'const FFMPEG_BIN_PATH: &str = "${ffmpeg}/bin/ffmpeg";'
 
-                substituteInPlace src/interface.rs \
-                  --replace '"~/.ffmpeg/nnedi3_weights"'\
-                            '"${self.outputs.packages.${system}.nnedi_weights}"'
-              fi
+              substituteInPlace src/interface.rs \
+                --replace '"~/.ffmpeg/nnedi3_weights"'\
+                          '"${nnedi_weights}"'
             '';
 
-            nativeBuildInputs = with pkgs; [
+            nativeBuildInputs = [
               pkg-config
               rustPlatform.bindgenHook
             ];
@@ -62,29 +72,25 @@
 
             inherit ffmpeg;
 
-            meta = with pkgs.lib; {
+            meta = with lib; {
               license = licenses.mpl20;
               homepage = "https://github.com/Sciencentistguy/videoconverter";
               platforms = ffmpeg.meta.platforms;
             };
           };
       in {
-        packages.nnedi_weights = builtins.fetchurl {
-          url = "https://github.com/dubhater/vapoursynth-nnedi3/raw/cc6f6065e09c9241553cb51f10002a7314d66bfa/src/nnedi3_weights.bin";
-          sha256 = "0hhx4n19qaj3g68f5kqjk23cj063g4y2zidivq9pdfrm0i1q5wr7";
-        };
-
         packages.videoconverter = pkgs.callPackage videoconverter {
           ffmpeg = pkgs.ffmpeg_5-full.override {
             nonfreeLicensing = true;
             fdkaacExtlib = true;
           };
         };
-
         # Assume that ffmpeg-full works and I don't need to build it in CI
         packages.videoconverter-ci = pkgs.callPackage videoconverter {
           ffmpeg = pkgs.ffmpeg_5;
         };
+
+        packages.nnedi_weights = nnedi_weights;
 
         packages.default = self.packages.${system}.videoconverter;
 
@@ -95,10 +101,8 @@
               clippy
               rustfmt
               cargo-edit
-              rustc
             ];
-
-          RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
         });
       });
 }
